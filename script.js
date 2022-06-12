7a398714d7f78f7e587e0536d476d03aaed51d5f
DINKONDBD <70340226+DINKONDBD@users.noreply.github.com>
2022-06-12 18:49:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,8 @@ function calculate (){
 function cleartxt(){
     document.getElementById('osc_screen').value = '';
 }
+function removelast() {
+    let rl = document.getElementById("osc_screen").value
+    let rt = rl.slice(0, -1);
+    document.getElementById("osc_screen").value = rt;
+}

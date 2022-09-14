de25d3a5b0bc898391c79104a1acb731da084e46
DINKONDBD <70340226+DINKONDBD@users.noreply.github.com>
2022-09-14 20:34:34 +0300

    d

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let result;
 function addtoinput(value){
-    document.getElementById("osc_screen").value += value;
+    document.getElementById("osc_screen").innerHTML += value;
 }
 function calculate (){
     window.onerror = function (e) {
         document.getElementById("osc_screen").value = e;
     };
-    result = eval(document.getElementById("osc_screen").value)
-    document.getElementById("osc_screen").value = result
+    result = eval(document.getElementById("osc_screen").innerHTML)
+    document.getElementById("osc_screen").innerHTML = result
 }
 function cleartxt(){
     document.getElementById('osc_screen').value = '';
 }
 function removelast() {
-    let rl = document.getElementById("osc_screen").value
+    let rl = document.getElementById("osc_screen").innerHTML
     let rt = rl.slice(0, -1);
     document.getElementById("osc_screen").value = rt;
 }
